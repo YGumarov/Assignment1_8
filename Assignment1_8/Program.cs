1c3834265d123d2d7a175a7b07ad1dd13880faa4
YGumarov <yerassylgumarov@gmail.com>
2023-04-09 15:21:06 +0600

    Initial commit

---
+++ Assignment1_8/Program.cs
@@ -0,0 +1,27 @@
+﻿static bool check(string stroka)
+{
+    if (stroka.Length == 0)
+    {
+        return false; 
+    }
+    else if (stroka.Length == 1)
+    {
+        return Char.IsDigit(stroka[0]); 
+    }
+    else
+    {
+        return Char.IsDigit(stroka[0]) && check(stroka.Substring(1));
+    }
+}
+
+string stroka = Console.ReadLine();
+bool result = check(stroka);
+
+if (result == true)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
